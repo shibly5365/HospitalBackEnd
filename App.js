2a783ff6274c4cd17907acc81dfd00e46ff6d5e9
shibly5365 <nuhm8565@gmail.com>
2026-01-02 15:47:26 +0530

    add the latst app.js file

--- App.js
+++ App.js
@@ -3,22 +3,26 @@ import connectDB from "./Config/db.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import http from "http";
 
 import routing from "./Routing/auth/AuthRotuing.js";
 import PatientRouting from "./Routing/Patient/Patient.js";
 import SuperAdminRouting from "./Routing/SuperAdmin/SuperAdminRouting.js";
 import AdminRouting from "./Routing/Admin/AadminRouting.js";
-import DoctorRouting from "./Routing/Doctor/DoctorRouiting.js";
+import DoctorRouting  from "./Routing/Doctor/DoctorRouiting.js"
 import ReceptionistRouting from "./Routing/Receptionist/ReceptionistRouting.js";
+import { initSocket } from "./Sockets/socketServer.js";
 
 dotenv.config();
 
 const app = express();
 
-//ORS configuration
+//CORS configuration
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: FRONTEND_URL,
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
@@ -39,8 +43,10 @@ app.use("/api/receptionist", ReceptionistRouting);
 //  Connect to MongoDB
 connectDB();
 
-//  Start server
+//  Start server with Socket.IO
 const PORT = process.env.PORT || 4002;
-app.listen(PORT, () => {
+const server = http.createServer(app);
+initSocket(server);
+server.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
